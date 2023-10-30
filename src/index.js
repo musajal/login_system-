f069b6201e9d06723a5973a1a6c5d0b5ba915d1f
Musa Jallow <apple@Apples-MacBook-Pro.local>
2023-10-30 09:54:04 +0000

    final changes to loging

--- src/index.js
+++ src/index.js
@@ -1,8 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import { disableReactDevtools} from '@fvilers/disable-react-devtools'
-if(process.env.NODE_ENV === 'production') disableReactDevtools()
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
